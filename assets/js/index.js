032c59a6f0d4fe7ba078eb69de09da45977237b5
Renan-Maestre <renan.maestre.dev@gmail.com>
2025-02-21 07:25:28 -0300

    Aula 3 - NLW Full Stack - introdução a estilização com css

--- assets/js/index.js
+++ assets/js/index.js
@@ -88,19 +88,75 @@ const formAction = () => {
     }
 }
 
+
+const updateImageLink = () =>{
+    document.querySelectorAll('img').forEach((img) =>{
+        img.src = `https://raw.githubusercontent.com/maykbrito/my-public-files/main/nlw-19/logo.svg`
+    })
+}
+
 const startApp = () =>{
     const content =  `
-    <form id="form">
-    <input type="email" name="email" placeholder="E-mail" />
-    <input type="text" name="phone" placeholder="Telefone" />
-        <button>
-            Confirmar
-        <button/>
-    <form/>
+    <header>
+        <img src="logo.svg" alt="logo">
+        <h1>
+            <span>CodeCraft</span><br>
+            Summit 2025
+        </h1>
+    </header>
+    <main>
+
+      <section class="about">
+        <div class="section-header">
+          <h2>Sobre o evento</h2>
+          <span>AO VIVO</span>
+
+          <p>
+            Um evento feito por e para desenvolvedores apaixonados por criar soluções inovadoras e compartilhar conhecimento. Vamos mergulhar nas últimas tendências em desenvolvimento de software, arquitetura de sistemas e tecnologias emergentes, com palestras, workshops e hackathons. <br>
+            15 a 17 de março | Das 18h às 21h |
+            Online & Gratuito
+          </p>
+        </div>
+
+      </section>
+
+      <section class="registration">
+        <h2>Inscrição</h2>
+
+        <form id="form">
+          <div class="input-wrapper">
+            <div class="input-group">
+              <label for="email">
+                <img src="https://raw.githubusercontent.com/maykbrito/my-public-files/main/nlw-19/mail.svg" alt="Email icon">
+              </label>
+              <input type="email" id="email" name="email" placeholder="E-mail">
+            </div>
+
+
+            <div class="input-group">
+              <label for="phone">
+                <img src= "https://raw.githubusercontent.com/maykbrito/my-public-files/main/nlw-19/phone.svg" alt="phone icon">
+
+              </label>
+              <input type="phone" id="phone" name="phone" placeholder="Telefone">
+            </div>
+
+
+
+          </div>
+          <button>
+            Cofirmar
+            <img src="arrow.sgv" alt="Arrow right" class="src">
+          </button>
+
+        </form>
+      </section>
+    </main>
+    <footer></footer>
     `
 
     app.innerHTML = content
-
+    updateImageLink()
     formAction()
 }
 startApp()
